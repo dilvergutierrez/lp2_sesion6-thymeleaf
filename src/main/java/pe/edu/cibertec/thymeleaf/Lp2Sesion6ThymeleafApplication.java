3df3c2311028ccc5fc9b2ba8560683f6e07facbb
dilver.gutierrez@gmail.com <dilver.gutierrez@gmail.com>
2024-10-27 00:13:29 -0500

    Primera version

---
+++ src/main/java/pe/edu/cibertec/thymeleaf/Lp2Sesion6ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2Sesion6ThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2Sesion6ThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/thymeleaf/Lp2Sesion6ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2Sesion6ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
